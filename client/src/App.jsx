4ca6518a7f5224fb8801877b5a5215c739930ef5
Sahas-cls <sahasdeneth20011022@gmail.com>
2025-08-28 07:57:44 +0530

    fixing git cannot be pull issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './components/Login';
-import Sidebar from './components/common/Sidebar';
-import Header from './components/Header';
-import PageDashboard from './components/pages/PageDashboard';
-import PageAddStudent from './components/pages/PageAddStudent';
-import ViewStudents from './components/ViewStudents';
-import PageViewStudent from './components/pages/PageViewStudent';
-import PageCourses from './components/pages/PageCourses';
-import PageManageUsers from './components/pages/PageManageUsers';
+import Login from "./components/Login";
+import Sidebar from "./components/common/Sidebar";
+import Header from "./components/Header";
+import PageDashboard from "./components/pages/PageDashboard";
+import PageAddStudent from "./components/pages/PageAddStudent";
+import ViewStudents from "./components/ViewStudents";
+import PageViewStudent from "./components/pages/PageViewStudent";
+import PageCourses from "./components/pages/PageCourses";
+import PageManageUsers from "./components/pages/PageManageUsers";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/test' element={<Header />} />
-        <Route path='/dashboard' element={<PageDashboard />} />
-        <Route path='/student/add' element={<PageAddStudent />} />
-        <Route path='/student/view' element={<PageViewStudent />} />
-        <Route path='/courses' element={<PageCourses />} />
-        <Route path='/users' element={<PageManageUsers />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/test" element={<Header />} />
+        <Route path="/dashboard" element={<PageDashboard />} />
+        <Route path="/student/add" element={<PageAddStudent />} />
+        <Route path="/student/view" element={<PageViewStudent />} />
+        <Route path="/courses" element={<PageCourses />} />
+        <Route path="/users" element={<PageManageUsers />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
